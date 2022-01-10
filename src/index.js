07e98bae57a6e576907e6f59b09d5fa86416cb68
Jerry Fitzner <jfitzner1@gmail.com>
2022-01-10 15:22:21 -0600

    Completted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const btn = document.createElement('button');
+    btn.textContent = 'X'
+    btn.addEventListener('click', deleteTask)
+    const list = document.getElementById("new-task-description");
+    
+    const listItem = document.createElement("li");
+    listItem.innerText = `${list.value}  `;
+    listItem.appendChild(btn);
+
+    append(listItem);
+    form.reset();
+  });
+
+  const append = function(item){
+    document.getElementById('tasks').appendChild(item);
+  };
+
+  const deleteTask = e => {
+    e.target.parentNode.remove();
+  }
 });
